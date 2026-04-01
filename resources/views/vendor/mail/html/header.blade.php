98961d0d769e9e2bd9dd43016887ec3c093f8157
Cédric <limacedric@hotmail.fr>
2026-04-01 13:38:16 +0200

    fix

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
 <tr>
 <td width="50%" align="left">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('/assets/logo_fitnessclic.png') }}" class="logo" width="150px" alt="FitnessClic Logo">
+<img src="{{ asset('/assets/logo_fitnessclic.png') }}" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; max-width: 100%; border: none; height: 80px; max-height: 80px; width: 80px;" class="logo" width="150px" alt="FitnessClic Logo">
 </a>
 </td>
 <td width="50%" align="right">
